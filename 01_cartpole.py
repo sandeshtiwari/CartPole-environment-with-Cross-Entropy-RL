41bca2321d6270fc86481b661728ca1b70ac594d
Sandesh Tiwari <sandeshtiwari@live.com>
2019-07-11 00:03:55 -0500

    CartPole environment RL with Cross-Entropy

---
+++ 01_cartpole.py
@@ -0,0 +1,108 @@
+#!/home/sandesh/rl/bin/python3
+import gym
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 16
+PERCENTILE = 70
+
+
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+Episode = namedtuple('Episode', field_names=['reward', 'steps'])
+EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
+
+
+def iterate_batches(env, net, batch_size):
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs = env.reset()
+    sm = nn.Softmax(dim=1)
+    while True:
+        obs_v = torch.FloatTensor([obs])
+        act_probs_v = sm(net(obs_v))
+        act_probs = act_probs_v.data.numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, is_done, _ = env.step(action)
+        episode_reward += reward
+        episode_steps.append(EpisodeStep(observation=obs, action=action))
+        if is_done:
+            batch.append(Episode(reward=episode_reward, steps=episode_steps))
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs = env.reset()
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        obs = next_obs
+
+
+def filter_batch(batch, percentile):
+    rewards = list(map(lambda episode: episode.reward, batch))
+    reward_bound = np.percentile(rewards, percentile)
+    reward_mean = float(np.mean(rewards))
+
+    train_obs = []
+    train_act = []
+    for episode in batch:
+        if episode.reward < reward_bound:
+            continue
+        train_obs.extend(map(lambda step: step.observation, episode.steps))
+        train_act.extend(map(lambda step: step.action, episode.steps))
+
+    train_obs_v = torch.FloatTensor(train_obs)
+    train_act_v = torch.LongTensor(train_act)
+    # return only the observation and action which are greater then or equal to the reward_bound
+    return train_obs_v, train_act_v, reward_bound, reward_mean
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    env = gym.wrappers.Monitor(env, directory="mon", force=True)
+    obs_size = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    net = Net(obs_size, HIDDEN_SIZE, n_actions)
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.01)
+    writer = SummaryWriter(comment="-cartpole")
+
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+    	# obs_v and acts_v are the observations and actions that give reward greater or higher to reward_b
+    	# acts_v acts like the 'y' mapping for our neural network
+        obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE)
+        optimizer.zero_grad()
+        # getting the action from the NN
+        action_scores_v = net(obs_v)
+        # Finding the loss between the NN's predicted output and the acts_v(which contains actions that yield high rewards)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+        print("%d: loss=%.3f, reward_mean=%.1f, reward_bound=%.1f" % (
+            iter_no, loss_v.item(), reward_m, reward_b))
+        writer.add_scalar("loss", loss_v.item(), iter_no)
+        writer.add_scalar("reward_bound", reward_b, iter_no)
+        writer.add_scalar("reward_mean", reward_m, iter_no)
+        if reward_m > 199:
+            print("Solved!")
+            break
+    writer.close()
